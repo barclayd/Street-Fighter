311ac28232073b2bc50e112a3783e030361c3f84
Daniel Barclay <barclaysd@me.com>
2019-06-05 21:37:52 +0100

    Game updates to dynamically display impact of user actions on player and opponent

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
-    <Healthbars />
-    <Actions />
-    <ActionLog />
+    <healthbars
+      :player-health="playerHealth"
+      :opponent-health="opponentHealth"
+    />
+    <actions
+      :is-running="isRunning"
+      @onUpdateGameStatus="onUpdateGameStatus"
+      @onUpdateHealth="onUpdateHealth"
+      @onHealUser="onHealUser"
+      :player-health="playerHealth"
+      :opponent-health="opponentHealth"
+    />
+    <actionLog />
   </div>
 </template>
 
@@ -19,6 +29,44 @@ export default {
     Healthbars,
     Actions,
     ActionLog,
+  },
+  data() {
+    return {
+      isRunning: false,
+      playerHealth: 80,
+      opponentHealth: 100,
+    };
+  },
+  methods: {
+    onUpdateGameStatus(status) {
+      this.isRunning = status;
+    },
+    onUpdateHealth(char, damage) {
+      if (char === 'player' && this.playerHealth > 0) {
+        this.playerHealth -= damage;
+        if (this.playerHealth < 0) {
+          this.playerHealth = 0;
+        }
+      } else if (char === 'opponent' && this.opponentHealth > 0) {
+        this.opponentHealth -= damage;
+        if (this.opponentHealth < 0) {
+          this.opponentHealth = 0;
+        }
+      }
+    },
+    onHealUser(char, healAmount) {
+      if (char === 'player') {
+        this.playerHealth += healAmount;
+        if (this.playerHealth > 100) {
+          this.playerHealth = 100;
+        }
+      } else if (char === 'opponent') {
+        this.opponentHealth += healAmount;
+        if (this.opponentHealth > 100) {
+          this.opponentHealth = 100;
+        }
+      }
+    }
   },
 };
 </script>
