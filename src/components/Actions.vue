078f806c649d4259ab7b5f3767c231cd53b06b61
Daniel Barclay <barclaysd@me.com>
2019-06-05 17:59:43 +0100

    Organised code into Vue components

---
+++ src/components/Actions.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <section class="row controls">
+      <div class="small-12 columns">
+        <button id="start-game">START NEW GAME</button>
+      </div>
+    </section>
+    <section class="row controls">
+      <div class="small-12 columns">
+        <button id="attack">Punch</button>
+        <button id="special-attack">Kick</button>
+        <button id="heal">HEAL</button>
+        <button id="give-up">GIVE UP</button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Actions',
+};
+</script>
+
+<style scoped>
+button {
+  font-size: 20px;
+  background-color: #eee;
+  padding: 12px;
+  box-shadow: 0 1px 1px black;
+  margin: 10px;
+  outline: none;
+}
+</style>
---
+++ src/components/Healthbars.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <section class="row">
+      <div class="small-6 columns">
+        <h1 class="text-center">You</h1>
+        <div class="healthbar">
+          <div
+            class="healthbar text-center"
+            style="background-color: green; margin: 0; color: white;"
+          ></div>
+        </div>
+      </div>
+      <div class="small-6 columns">
+        <h1 class="text-center">Opponent</h1>
+        <div class="healthbar">
+          <div
+            class="healthbar text-center"
+            style="background-color: green; margin: 0; color: white;"
+          ></div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Healthbar',
+};
+</script>
+
+<style scoped></style>
