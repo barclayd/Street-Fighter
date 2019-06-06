46ba69cebee1720c57ad582025f4fc459654cc18
Daniel Barclay <daniel@urbanintel.io>
2019-06-06 20:45:34 +0100

    Check who wins, opponent move calculated and ends/resets game

--- src/components/Actions.vue
+++ src/components/Actions.vue
@@ -2,14 +2,34 @@
   <div>
     <section class="row controls" v-if="!isRunning">
       <div class="small-12 columns">
-        <button id="start-game" @click="onUpdateGameStatus(true)">START NEW GAME</button>
+        <button id="start-game" @click="onUpdateGameStatus(true)">
+          START NEW GAME
+        </button>
       </div>
     </section>
     <section class="row controls" v-else>
       <div class="small-12 columns">
-        <button id="attack" :disabled="parseInt(opponentHealth) === 0" @click="userMove('attack')">PUNCH</button>
-        <button id="special-attack" @click="userMove('specialAttack')">KICK</button>
-        <button id="heal" @click="userMove('heal')">HEAL</button>
+        <button
+          id="attack"
+          :disabled="parseInt(opponentHealth) < 0"
+          @click="userMove('attack')"
+        >
+          PUNCH
+        </button>
+        <button
+          id="special-attack"
+          :disabled="parseInt(opponentHealth) < 1"
+          @click="userMove('specialAttack')"
+        >
+          KICK
+        </button>
+        <button
+          id="heal"
+          :disabled="parseInt(playerHealth) < 1"
+          @click="userMove('heal')"
+        >
+          HEAL
+        </button>
         <button id="give-up" @click="onUpdateGameStatus(false)">GIVE UP</button>
       </div>
     </section>
@@ -21,37 +41,63 @@ export default {
   name: 'Actions',
   props: ['is-running', 'player-health', 'opponent-health'],
   methods: {
+    checkWinner(user, damageTaken) {
+      if (user === 'opponent' && this.$props.opponentHealth-damageTaken <= 0) {
+        setTimeout(() => {
+          alert('You win! Opponent is dead. \n Game over!');
+        }, 600);
+        this.onUpdateGameStatus(false);
+      }
+      if (user === 'player' && this.$props.playerHealth-damageTaken <= 0) {
+        setTimeout(() => {
+          alert('You lose! Opponent is the winner. \n Game over!');
+        }, 600);
+          this.onUpdateGameStatus(false);
+      }
+    },
     onUpdateGameStatus(status) {
       this.$emit('onUpdateGameStatus', status);
     },
     heal(char, min, max) {
       const healAmount = Math.max(Math.floor(Math.random() * max) + 1, min);
-      this.$emit('onHealUser', char, healAmount)
+      this.$emit('onHealUser', char, healAmount);
     },
     damage(min, max, user) {
       const damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-      if (user === 'player') {
+      if (user === 'player' && this.$props.playerHealth >= 0) {
         this.$emit('onUpdateHealth', 'opponent', damage);
-      } else {
+        this.checkWinner('opponent', damage);
+      } else if (this.$props.opponentHealth >= 0){
         this.$emit('onUpdateHealth', 'player', damage);
+        this.checkWinner('player', damage);
       }
     },
-    userMove(move) {
-      switch (move) {
-        case 'attack':
-          this.damage(3, 10, 'player');
-          break;
-        case 'specialAttack':
-          this.damage(6, 20, 'player');
-          break;
-        case 'heal':
-          this.heal('player', 4, 16);
-          break;
-        default:
-          return;
+    async opponentMove(min, max) {
+      const responseTime = Math.max(Math.floor(Math.random() * max) + 1, min);
+      await setTimeout(() => {
+        this.damage(5, 17, 'opponent');
+      }, responseTime);
+    },
+    async userMove(move) {
+      if (this.$props.playerHealth > 0) {
+        switch (move) {
+          case 'attack':
+            this.damage(3, 10, 'player');
+            await this.opponentMove(300, 1000);
+            break;
+          case 'specialAttack':
+            this.damage(6, 20, 'player');
+            await this.opponentMove(500, 1500);
+            break;
+          case 'heal':
+            this.heal('player', 4, 16);
+            break;
+          default:
+            return;
+        }
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
